081c0c3ab0dd4fc6630a364acf6960569b753e75
Artem Olkov <artem.olkov@jetbrains.com>
2025-07-09 10:06:50 +0200

    KT-78604: remove KlibScope as it deemed unneeded ^KT-78604 fixed

--- native/swift/swift-export-standalone/build.gradle.kts
+++ native/swift/swift-export-standalone/build.gradle.kts
@@ -24,34 +24,10 @@ dependencies {
     implementation(project(":analysis:analysis-api-standalone"))
 
     implementation(project(":native:analysis-api-klib-reader"))
-
-    testApi(platform(libs.junit.bom))
-    testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.junit.jupiter.api)
-
-    testRuntimeOnly(projectTests(":analysis:low-level-api-fir"))
-    testRuntimeOnly(projectTests(":analysis:analysis-api-impl-base"))
-    testImplementation(projectTests(":analysis:analysis-api-fir"))
-    testImplementation(projectTests(":analysis:analysis-test-framework"))
-    testImplementation(projectTests(":compiler:tests-common"))
-    testImplementation(projectTests(":compiler:tests-common-new"))
-
-    if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
-        testApi(projectTests(":native:native.tests"))
-    }
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" {
-        projectDefault()
-        generatedTestDir()
-    }
-}
-
-val testTags = findProperty("kotlin.native.tests.tags")?.toString()
-val test by nativeTest("test", testTags) {
-    dependsOn(":kotlin-native:distInvalidateStaleCaches")
 }
 
 publish()
@@ -59,5 +35,3 @@ publish()
 runtimeJar()
 sourcesJar()
 javadocJar()
-
-testsJar()
