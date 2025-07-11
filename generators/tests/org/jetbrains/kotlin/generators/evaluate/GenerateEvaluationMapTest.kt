9bff73a772f3c4473c640b1a9946d399539b0d30
Florian Freitag <florian.freitag@jetbrains.com>
2025-07-11 16:41:25 +0200

    Add tests for backend evaluation of unsigned types
    KT-51065

--- generators/tests/org/jetbrains/kotlin/generators/evaluate/GenerateEvaluationMapTest.kt
+++ generators/tests/org/jetbrains/kotlin/generators/evaluate/GenerateEvaluationMapTest.kt
@@ -3,14 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.generators.test.interpreter
+package org.jetbrains.kotlin.generators.test.evaluate
 
 import junit.framework.TestCase
-import org.jetbrains.kotlin.generators.interpreter.DESTINATION
-import org.jetbrains.kotlin.generators.interpreter.generateMap
+import org.jetbrains.kotlin.generators.evaluate.DESTINATION
+import org.jetbrains.kotlin.generators.evaluate.generateMap
 import org.jetbrains.kotlin.test.KotlinTestUtils
 
-class GenerateInterpreterMapTest : TestCase() {
+class GenerateEvaluationMapTest : TestCase() {
     fun testGeneratedDataIsUpToDate() {
         val text = generateMap()
         KotlinTestUtils.assertEqualsToFile(DESTINATION, text)
