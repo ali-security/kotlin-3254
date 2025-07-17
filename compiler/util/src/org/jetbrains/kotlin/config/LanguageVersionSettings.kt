85a6844c5fed0222328c2a997e049179e1406ad4
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2025-07-17 11:02:47 +0300

    fixup! Add `ForbidBridgesConflictingWithInheritedMethodsInJvmCode` feature
    The feature is added to introduce [ACCIDENTAL_OVERRIDE_BY_BRIDGE_METHOD]
    deprecation error.

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -446,11 +446,11 @@ enum class LanguageFeature(
     ForbidReturnInExpressionBodyWithoutExplicitTypeEdgeCases(KOTLIN_2_4, enabledInProgressiveMode = true, "KTLC-288"),
     CheckOptInOnPureEnumEntries(KOTLIN_2_4, enabledInProgressiveMode = true, "KTLC-359"),
     ForbidExposingPackagePrivateInInternal(KOTLIN_2_4, enabledInProgressiveMode = true, "KTLC-271"),
-    ForbidBridgesConflictingWithInheritedMethodsInJvmCode(KOTLIN_2_4, enabledInProgressiveMode = true, "KT-13712"),
 
     // 2.5
 
     ErrorAboutDataClassCopyVisibilityChange(KOTLIN_2_5, enabledInProgressiveMode = true, "KT-11914"), // KT-11914. Deprecation phase 2
+    ForbidBridgesConflictingWithInheritedMethodsInJvmCode(KOTLIN_2_5, enabledInProgressiveMode = true, "KT-13712"),
 
     // End of 2.* language features --------------------------------------------------
 
