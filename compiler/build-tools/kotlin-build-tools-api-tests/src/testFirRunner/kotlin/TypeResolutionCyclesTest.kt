ramccor/kotlin

[BTA] Allow using v2 API on v1-only compiler through compatibility adapter
^KT-78196 Fixed
^KT-78195 Fixed

compiler/build-tools/kotlin-build-tools-api-tests/src/testFirRunner/kotlin/TypeResolutionCyclesTest.kt
@@ -3,12 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-import org.jetbrains.kotlin.buildtools.api.CompilerExecutionStrategyConfiguration
-import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.BaseCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.assertions.assertCompiledSources
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.model.Module
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.scenario.scenario
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.util.moduleWithFir
+import org.jetbrains.kotlin.buildtools.api.v2.ExecutionPolicy
+import org.jetbrains.kotlin.buildtools.api.v2.KotlinToolchain
 import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
@@ -21,15 +23,9 @@ class TypeResolutionCyclesTest : BaseCompilationTest() {
     @DefaultStrategyAgnosticCompilationTest
     @DisplayName("Potential first-round errors: cyclic dependency created")
     @TestMetadata("empty")
-    fun testCyclicDependencyCreated(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        scenario(strategyConfig) {
-            val module = module(
-                "empty",
-                additionalCompilationArguments = listOf("-Xuse-fir-ic"),
-                incrementalCompilationOptionsModifier = { incrementalOptions ->
-                    (incrementalOptions as ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration).useFirRunner(true)
-                }
-            )
+    fun testCyclicDependencyCreated(kotlinToolchain: KotlinToolchain, executionPolicy: ExecutionPolicy) {
+        scenario(kotlinToolchain, executionPolicy) {
+            val module = moduleWithFir("empty")
 
             module.createFile(
                 "File1.kt",

compiler/build-tools/kotlin-build-tools-api-tests/src/testFirRunner/kotlin/TypealiasChangeTest.kt
@@ -3,12 +3,13 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-import org.jetbrains.kotlin.buildtools.api.CompilerExecutionStrategyConfiguration
-import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.BaseCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.assertions.assertCompiledSources
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.scenario.scenario
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.util.moduleWithFir
+import org.jetbrains.kotlin.buildtools.api.v2.ExecutionPolicy
+import org.jetbrains.kotlin.buildtools.api.v2.KotlinToolchain
 import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
@@ -21,15 +22,9 @@ class TypealiasChangeTest : BaseCompilationTest() {
     @DefaultStrategyAgnosticCompilationTest
     @DisplayName("Potential first-round errors: typealias change")
     @TestMetadata("empty")
-    fun testTypealiasChange(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        scenario(strategyConfig) {
-            val module = module(
-                "empty",
-                additionalCompilationArguments = listOf("-Xuse-fir-ic"),
-                incrementalCompilationOptionsModifier = { incrementalOptions ->
-                    (incrementalOptions as ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration).useFirRunner(true)
-                }
-            )
+    fun testTypealiasChange(kotlinToolchain: KotlinToolchain, executionPolicy: ExecutionPolicy) {
+        scenario(kotlinToolchain, executionPolicy) {
+            val module = moduleWithFir("empty")
 
             module.createFile(
                 "Foo.kt",

ali-security/kotlin-3254

compiler/build-tools/kotlin-build-tools-api-tests/src/testFirRunner/kotlin/TypeResolutionCyclesTest.kt
@@ -3,12 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-import org.jetbrains.kotlin.buildtools.api.CompilerExecutionStrategyConfiguration
-import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.api.ExecutionPolicy
+import org.jetbrains.kotlin.buildtools.api.KotlinToolchain
+import org.jetbrains.kotlin.buildtools.api.tests.CompilerExecutionStrategyConfiguration
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.BaseCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.assertions.assertCompiledSources
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.scenario.scenario
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.util.moduleWithFir
 import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
@@ -23,13 +25,7 @@ class TypeResolutionCyclesTest : BaseCompilationTest() {
     @TestMetadata("empty")
     fun testCyclicDependencyCreated(strategyConfig: CompilerExecutionStrategyConfiguration) {
         scenario(strategyConfig) {
-            val module = module(
-                "empty",
-                additionalCompilationArguments = listOf("-Xuse-fir-ic"),
-                incrementalCompilationOptionsModifier = { incrementalOptions ->
-                    (incrementalOptions as ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration).useFirRunner(true)
-                }
-            )
+            val module = moduleWithFir("empty")
 
             module.createFile(
                 "File1.kt",

compiler/build-tools/kotlin-build-tools-api-tests/src/testFirRunner/kotlin/TypealiasChangeTest.kt
@@ -3,12 +3,14 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-import org.jetbrains.kotlin.buildtools.api.CompilerExecutionStrategyConfiguration
-import org.jetbrains.kotlin.buildtools.api.jvm.ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration
+import org.jetbrains.kotlin.buildtools.api.ExecutionPolicy
+import org.jetbrains.kotlin.buildtools.api.KotlinToolchain
+import org.jetbrains.kotlin.buildtools.api.tests.CompilerExecutionStrategyConfiguration
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.BaseCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.assertions.assertCompiledSources
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.api.tests.compilation.scenario.scenario
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.util.moduleWithFir
 import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
@@ -23,13 +25,7 @@ class TypealiasChangeTest : BaseCompilationTest() {
     @TestMetadata("empty")
     fun testTypealiasChange(strategyConfig: CompilerExecutionStrategyConfiguration) {
         scenario(strategyConfig) {
-            val module = module(
-                "empty",
-                additionalCompilationArguments = listOf("-Xuse-fir-ic"),
-                incrementalCompilationOptionsModifier = { incrementalOptions ->
-                    (incrementalOptions as ClasspathSnapshotBasedIncrementalJvmCompilationConfiguration).useFirRunner(true)
-                }
-            )
+            val module = moduleWithFir("empty")
 
             module.createFile(
                 "Foo.kt",